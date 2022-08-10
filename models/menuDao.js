62ac73fed63e18fdeedf4fe12e25f1626627dac6
dingwan <dingwan0331@gmail.com>
2022-08-10 22:40:18 +0900

    CREATE : 상품리스트
    - query params 로 offset & limit 기능구현
    - offset=0 , limit=5로 default값 지정
    - ordering은 랜덤으로 고정

---
+++ models/menuDao.js
@@ -0,0 +1,56 @@
+const myDataSource    = require('../config/database/mysql')
+const { CreateError } = require('../utils/Exceptions')
+
+const getmenus = async (offset, limit) => {
+    try{
+        const menus = await myDataSource.query(
+            `SELECT 
+            menus.id,
+            categories.name AS category,
+            menus.name,
+            menus.description,
+            menus.is_sold AS isSold,
+            badges.name AS badge
+            FROM menus 
+            LEFT JOIN categories ON menus.category_id=categories.id
+            LEFT JOIN badges ON menus.badge_id=badges.id 
+            ORDER BY rand() LIMIT ${limit} OFFSET ${offset};`
+        )
+
+        let menusId = []
+
+        if (!menus.length){return menus}
+
+        menus.forEach(element => {
+            menusId.push(element.id)
+        })
+
+        const items = await myDataSource.query(
+            `SELECT items.id,
+            menus.id AS menuId,
+            sizes.kor_name AS name,
+            sizes.name AS size,
+            items.price,
+            items.is_sold AS isSold
+            FROM items
+            LEFT JOIN sizes ON items.size_id=sizes.id
+            LEFT JOIN menus ON items.menu_id=menus.id
+            WHERE items.menu_id IN (SELECT menus.id FROM menus WHERE menus.id IN (${menusId}))`
+        )
+
+        const tags = await myDataSource.query(
+            `SELECT tags.id,
+            menus.id AS menuId,
+            tags.name,
+            tag_types.name AS type
+            FROM tags
+            LEFT JOIN tag_types ON tags.type_id=tag_types.id
+            LEFT JOIN menus ON menus.tag_id=tags.id
+            WHERE tags.id IN (SELECT menus.tag_id FROM menus WHERE menus.id IN (${menusId}))`
+        )
+        return { menus, items, tags}
+
+    }catch(err){throw new CreateError(500,'Database Error')}
+}
+
+module.exports = { getmenus }
---
+++ services/menuService.js
@@ -0,0 +1,38 @@
+const menuDao = require('../models/menuDao')
+const { CreateError } = require('../utils/Exceptions')
+
+const getmenus = async (offset, limit) => {
+    offset = offset ? offset : 0
+    limit  = limit ? limit : 5 
+    
+    if (isNaN(+offset) || isNaN(+limit)){
+        throw new CreateError(400,'Invalid Query')
+    }
+
+    const { menus, items, tags } = await menuDao.getmenus(offset, limit)
+    
+    let result = []
+    
+    if(!menus){ return retuls }
+    
+    menus.forEach(element => {
+        element.items = []
+        for(i=0;i<items.length;i++){
+            if(items[i].menuId == element.id){
+                element.items.push(items[i])
+            }
+        }
+
+        element.tags = []
+        for(i=0;i<tags.length;i++){
+            if(tags[i].menuId == element.id){
+                element.tags.push(tags[i])
+            }
+        }
+        result.push(element)
+    });
+    
+    return result
+}
+
+module.exports = { getmenus }
